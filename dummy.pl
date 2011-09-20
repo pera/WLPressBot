e6c9bf281ab3f05b86c22af3240a4675f1bdfa2b
Brian Gomes Bascoy <gomesbascoy@gmail.com>
2011-09-20 14:12:28 -0300

    first commit :D

---
+++ dummy.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+
+sub brain {
+    my ($nick, $channel, $what) = @_;
+    
+    given($what){
+		### Commands ###
+		when (/!(title|t) http:\/\/(.+?)( |$)/)	{	send_me($channel, get_title($2) ); }
+
+		### Easter eggs ;D ###
+		when (/!hi/)		{	send_msg($channel, 'hello '.$nick.'! nice to see you :D'); }
+		when (/!shodan/)	{	send_msg($channel, 'Look at you, hacker. A pathetic creature of meat and bone, panting and sweating as you run through my corridors. How can you challenge a perfect, immortal machine?'); }
+		when (/!tron/)		{	send_msg($channel, 'This is the key to a new order. This code disk means freedom.'); }
+		when (/!joshua/)	{	send_msg($channel, 'Greetings, Professor Falken.'); }
+
+		### Undefined command reply ###
+		default {
+			my @r = ('sorry?', 'I\'m not sure what you mean', 'I don\'t understand...');
+			send_msg($channel, $r[rand @r]);
+		}
+    }
+}
+
+# esta rutina tiene aire de insegura, tener cuidado...
+sub get_title{
+	my $url = $_[0];
+	my $content = get('http://'.$url);
+	$content=~s/&amp;/&/g;
+	$content=~s/&nbsp;/ /g;
+	$content=~s/&quot;/"/g;
+	$content=~s/&reg;/(r)/g;
+	
+	if($content=~/<title>(.+?)<\/title>/s){
+		my $title=$1;
+		$title=~s/[\t|\n]//g;
+		return $title;
+	}
+	
+	return 'title tag not found';
+}
+
---
+++ wlpressbot.pl
@@ -0,0 +1,295 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature qw(switch);
+use LWP::Simple;
+use POE;
+use POE qw(Component::IRC);
+use POE::Component::IRC::Common qw( :ALL );
+use XML::Simple;
+use XML::Feed;
+use DateTime;
+
+# autoflush
+$|=1;
+
+if(!$ARGV[0]){
+	die "Unspecified channel";
+}
+
+my $version = '0.1';
+
+my $config = XMLin('config.xml');
+
+my $nickname = $config->{nick};
+my $username = $config->{user};
+my $server = $config->{server};
+my $port = $config->{port};
+# Si el servidor dispone de NickServ este es el password
+my $nickpass = $config->{nickpass};
+# Termino buscado
+my $term = $config->{term};
+# Tiempo de espera de actualizacion de feeds
+my $delay = $config->{delay};
+
+my $ircname = 'WLPressBot v'.$version.' - Everything is true - Everything is permissible!';
+
+my @channels = ('#'.$ARGV[0]);
+
+my $joined = 0;
+
+my $aggregator = Aggregator->new();
+
+open FEEDLIST, "feedlist" or die $!;
+while(<FEEDLIST>){
+	chomp;
+	$aggregator->addFeed($_);
+}
+close FEEDLIST;
+
+my $irc = POE::Component::IRC->spawn( 
+    nick => $nickname,
+    username => $username,
+    ircname => $ircname,
+    server => $server,
+	port => $port,
+	usessl => 1
+) or die "Oh noooo! $!";
+
+POE::Session->create( package_states => [ main => [ qw(_default _start irc_001 irc_public irc_msg irc_join irc_part irc_quit irc_kick irc_nick) ], ],	heap => { irc => $irc }, );
+
+POE::Session->create(
+	inline_states => {
+		_start => sub { $_[KERNEL]->yield("next") },
+		next   => sub {
+			if( $joined == 1 ){
+				for my $feed (@{$aggregator}) {
+					my $date = check_feed($feed->getURL(), $feed->getLastDate(), $term);
+					$feed->setLastDate($date);
+				}
+			}
+			$_[KERNEL]->delay(next => $delay);
+		},
+	},
+);
+
+$poe_kernel->run();
+
+sub check_feed {
+	my ($source, $last, $word) = @_;
+	
+	my $feed = XML::Feed->parse(URI->new($source))
+		or print XML::Feed->errstr;
+
+	#print $feed->title, "\n";
+
+	for my $entry ($feed->entries) {
+		last if( $entry->issued <= $last );
+		if($entry->content->body=~/(?i)$word/s) {
+			send_msg($channels[0], $entry->title." | ".$entry->link);
+		}
+	}
+
+	return ($feed->entries)[0]->issued;
+}
+
+# Class Feed
+{
+	package Feed;
+	
+	sub new {
+		my ($class, $url) = @_;
+		my $self = bless {}, $class;
+
+		$self->setURL($url);
+		$self->setLastDate(DateTime->new(year=>2600));
+
+		return $self;
+	}
+
+	sub setURL {
+		my ($self, $url) = @_;
+		$self->{ URL } = $url;
+	}
+
+	sub setLastDate {
+		my ($self, $lastdate) = @_;
+		$self->{ LASTDATE } = $lastdate;
+	}
+
+	sub getURL {
+		my ($self) = @_;
+		return $self->{ URL };
+	}
+
+	sub getLastDate {
+		my ($self) = @_;
+		return $self->{ LASTDATE };
+	}
+}
+
+# Class Aggregator
+{
+	package Aggregator;
+
+	sub new {
+		my ($class) = @_;
+		my $self = bless [], $class;
+
+		return $self;
+	}
+
+	sub addFeed {
+		my ($self, $url) = @_;
+		push @{$self}, Feed->new($url);
+	}
+}
+
+sub _start {
+    my $heap = $_[HEAP];
+    my $irc = $heap->{irc};
+    
+    $irc->yield( register => 'all' );
+    $irc->yield( connect => { } );
+    return;
+}
+
+sub irc_001 {
+    my $sender = $_[SENDER];
+	my $irc = $sender->get_heap();
+    
+    print "Connected to ", $irc->server_name(), "\n";
+	
+	$irc->yield( nickserv => 'IDENTIFY' => $nickpass );
+    $irc->yield( join => $_ ) for @channels;
+    return;
+}
+
+sub irc_public {
+    my ($sender, $who, $where, $what) = @_[SENDER, ARG0 .. ARG2];
+    my $nick = ( split /!/, $who )[0];
+    my $channel = $where->[0];
+    
+   if($what=~m/!\w/){
+	  &evaluator;
+		eval brain($nick, $channel, $what);
+		if ($@) {
+			send_me($channel, "bawww Im dying D:");
+		}
+   }
+
+	print "irc_public: <$nick> $what\n";
+
+    return;
+}
+
+sub irc_msg {
+    my ($who, $recipients, $what) = @_[ARG0 .. ARG2];
+    my $nick = ( split /!/, $who )[0];
+
+	print "irc_msg: <$nick> $what\n";
+    
+	return;
+}
+
+sub irc_join {
+	my ($who, $where) = @_[ARG0, ARG1];
+	my $nick = ( split /!/, $who )[0];
+    my $channel = $where;
+    
+	print "irc_join: $nick\n";
+
+	$joined = 1;
+
+    return;
+}
+
+sub irc_part {
+	my ($who, $where) = @_[ARG0, ARG1];
+	my $nick = ( split /!/, $who )[0];
+    my $channel = $where;
+    
+	print "irc_part: $nick\n";
+	
+	return;
+}
+
+sub irc_quit {
+	my ($who, $where) = @_[ARG0, ARG1];
+	my $nick = ( split /!/, $who )[0];
+    my $channel = $where;
+    
+	print "irc_quit: $nick\n";
+	
+	return;
+}
+
+sub irc_kick {
+	my ($kicker, $where, $kickee) = @_[ARG0, ARG1, ARG2];
+    
+	print "irc_kick: $kicker kicked $kickee\n";
+	
+	return;
+}
+
+sub irc_nick {
+	my ($who, $newnick) = @_[ARG0, ARG1];
+	my $oldnick = ( split /!/, $who )[0];
+    
+	print "irc_nick: $oldnick changed to $newnick\n";
+	
+	return;
+}
+
+sub _default {
+    my ($event, $args) = @_[ARG0 .. $#_];
+    my @output = ( "$event:" );
+    
+    for my $arg (@$args) {
+        if ( ref $arg eq 'ARRAY' ) {
+            push( @output, '[' . join(', ', @$arg ) . ']' );
+        }
+        else {
+            push ( @output, "'$arg'" );
+        }
+    }
+    print join ' ', @output, "\n";
+    return 0;
+}
+
+sub evaluator{
+    my $dummy;
+    
+    open FD, "dummy.pl" or die $!;
+    while(<FD>){
+        $dummy .= $_;
+    }
+    close FD;
+ 
+	eval $dummy;
+	if ($@) {
+		print $@;
+	}
+}
+
+sub send_msg{
+    $irc->yield( privmsg => $_[0] => $_[1] );
+}
+
+sub send_me{
+    $irc->yield( ctcp => $_[0] => 'ACTION '.$_[1])
+}
+
+sub send_notice{
+    $irc->yield( notice => $_[0] => $_[1] );
+}
+
+sub set_mode{
+    $irc->yield( mode => $_[0] => $_[1] );
+}
+
+sub delay_event{
+	$irc->delay( $_[0], $_[1] );
+}
+
